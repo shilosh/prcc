e81be7028559e168c98e86226aaa4ab7191dc9f2
evyatar <evyatar@tikalk.com>
2023-08-15 10:34:18 +0300

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git checkout master && \
+(git branch -D dist || true) && \
+git checkout -b dist && \
+rm .gitignore && \
+npm run build && \
+cp dist/prccapp/index.html dist/prccapp/404.html && \
+git add dist/prccapp && \
+git commit -m dist && \
+(git branch -D gh-pages || true) && \
+git subtree split --prefix dist/prccapp -b gh-pages && \
+git push -f origin gh-pages:gh-pages && \
+git checkout master && \
+git branch -D gh-pages && \
+git branch -D dist && \
+git checkout . 
